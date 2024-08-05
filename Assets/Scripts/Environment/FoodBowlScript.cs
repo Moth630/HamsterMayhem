0f408fa9a92a63970079f1f5c3ff23f3c79be2bb
Dooho Han <smicdooho@live.co.kr>
2024-08-05 00:05:10 -0400

    InCage Level design
    designed level shapes and started baseclass for interactables

---
+++ Assets/Scripts/Environment/FoodBowlScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodBowlScript : MonoBehaviour
+//This is a foodbowl in the starting area
+//that'll constantly dispense food and allow
+//player to keep trying things
+//
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Environment/InteractableBaseClass.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract class InteractableBaseClass : MonoBehaviour
+{
+//base class for interactable environment items
+
+//method for pop up text
+
+//method for interaction
+
+//method for not interactable anymore
+
+//method for transitioning between
+
+
+}
