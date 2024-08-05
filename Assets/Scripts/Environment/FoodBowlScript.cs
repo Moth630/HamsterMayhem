218fbc93ed3f435e14a180d200c94f33523e8411
Dooho Han <smicdooho@live.co.kr>
2024-08-05 19:09:12 -0400

    blah

--- Assets/Scripts/Environment/FoodBowlScript.cs
+++ Assets/Scripts/Environment/FoodBowlScript.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FoodBowlScript : MonoBehaviour
+public class FoodBowlScript : InteractableBaseClass
 //This is a foodbowl in the starting area
 //that'll constantly dispense food and allow
 //player to keep trying things
 //
 {
+  [SerializeField] bool _StarterArea; //if starter area, will keep refilling
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +19,23 @@ public class FoodBowlScript : MonoBehaviour
     void Update()
     {
 
+    }
+    public override void PopUp()
+    {
+      Debug.Log("PopUp start!");
+      //code here to do canvas? TMPro around object that's always facing player with stylized letter
+      //"press e!" "do this!"
+    }
+    public override void AbleInteract(float _distanceNeeded, Vector3 _playerTransform)
+    {
+      PopUp();
+    }
+    public override void IsInteracting()
+    {
+      Debug.Log("");
+    }
+    public override void Broken()
+    {
+      Debug.Log("");
     }
 }
--- Assets/Scripts/Environment/InteractableBaseClass.cs
+++ Assets/Scripts/Environment/InteractableBaseClass.cs
@@ -2,16 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-abstract class InteractableBaseClass : MonoBehaviour
+public abstract class InteractableBaseClass : MonoBehaviour
 {
 //base class for interactable environment items
 
 //method for pop up text
-
-//method for interaction
-
-//method for not interactable anymore
-
+public virtual void PopUp()
+{
+  Debug.Log("PopUp start!");
+}
+//method for see if able to start interaction because player close
+public virtual void AbleInteract(float _distanceNeeded, Vector3 _playerTransform)
+{
+  PopUp();
+}
+//method for starting interaction
+//just switch this for Update() with a bool for ready to interact
+//method for while interacting? in update, if interacting is true do this
+public virtual void IsInteracting()
+{
+  Debug.Log("Interactioning!");
+}
+//method for not interactable anymore for things that can be broken/runout
+public abstract void Broken();
 //method for transitioning between
 
 
